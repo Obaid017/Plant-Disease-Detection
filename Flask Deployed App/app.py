da473f024d692e693ed032ec73e9a6ceb381d77e
Obaid017 <obaid0104049@gmail.com>
2026-05-18 08:59:11 +0500

    port 7860

--- Flask Deployed App/app.py
+++ Flask Deployed App/app.py
@@ -131,4 +131,6 @@ def market():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Hugging Face sets the target environment port to 7860 automatically
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
